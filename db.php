aa30b4812321f9f3d702e4019aba92f6def868b8
ahmetcelik43 <45917904+ahmetcelik43@users.noreply.github.com>
2021-02-08 00:06:24 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,6 +5,8 @@ $pass = "r=np?~OxV7-F";
 $db = "xlivacom_quiz";
 
 try {
+	   echo(12);die();
+
 	$db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 }catch(PDOException $e) {
 	echo $e->getMessage();
