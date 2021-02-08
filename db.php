1d8e244e399730a09548697ef066eb3511c55a50
ahmetcelik43 <45917904+ahmetcelik43@users.noreply.github.com>
2021-02-08 14:40:49 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
-$host = "ec2-52-50-171-4.eu-west-1.compute.amazonaws.com";
-$user = "jcqfmvskaxnsff";
-$pass = "5f916f074b1b63a7b4a91e469aa25ef14aaeaa028ec52e101e83baf02813de02";
-$db = "d9cq1i52lr8iaf";
+$host = "sql7.freemysqlhosting.net";
+$user = "sql7391574";
+$pass = "TmJxjaEdAd";
+$db = "sql7391574";
 
 try {
-	  $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
+//	  $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
 
-	//$db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-	$db = new PDO($dsn);
+	$db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+	//$db = new PDO($dsn);
 }catch(PDOException $e) {
 	echo $e->getMessage();
 }
